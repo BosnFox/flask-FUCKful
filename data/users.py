bcd5398883cb3e6bd45a7f1827dbda99b54dc3aa
Bosn_Fox <bogdanovplaton@gmail.com>
2022-05-09 18:10:16 +0300

    Ресурс Работы

--- data/users.py
+++ data/users.py
@@ -1,6 +1,7 @@
 import datetime
 import sqlalchemy
 from .db_session import SqlAlchemyBase
+from sqlalchemy_serializer import SerializerMixin
 
 
 class User(SqlAlchemyBase):
@@ -15,3 +16,18 @@ class User(SqlAlchemyBase):
     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     created_date = sqlalchemy.Column(sqlalchemy.DateTime,
                                      default=datetime.datetime.now)
+
+
+class Jobs(SqlAlchemyBase, SerializerMixin):
+    __tablename__ = 'jobs'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    team_leader = sqlalchemy.Column(sqlalchemy.Integer,
+                                    sqlalchemy.ForeignKey('users.id'))
+    job = sqlalchemy.Column(sqlalchemy.String)
+    work_size = sqlalchemy.Column(sqlalchemy.Integer)
+    collaborators = sqlalchemy.Column(sqlalchemy.String)
+    start_date = sqlalchemy.Column(sqlalchemy.String)
+    end_date = sqlalchemy.Column(sqlalchemy.String)
+    is_finished = sqlalchemy.Column(sqlalchemy.Boolean)
